8de27d3642f7b93738b202239b590f6d5823e2fa
Mohammad Sohrabi <shrbmamad@gmail.com>
2023-02-03 22:30:30 +0330

    add keys -bug fixed

--- DynamicHashtable.js
+++ DynamicHashtable.js
@@ -1,24 +1,32 @@
+import LinkList from "./LinkedList.js";
+
 class Node {
     constructor(key, value) {
         this.key = key;
         this.value = value;
     }
 }
+
 class DHashtable{
+    
     constructor(){
+        this.keys = new LinkList();
         this.hashSize = 0;
         this.arraySize = 10;
         this.array = new Array(10);
     }
+
     add(key, value) {
         let i = 0;
         let index = this.hashFunc(key, i++);
         while (this.array[index] !== undefined && this.array[index] !== null) {index = this.hashFunc(key, i++);
         }
         this.array[index] = new Node(key, value);
+        this.keys.add_last(key);
         this.hashSize += 1;
         if (this.hashSize === this.arraySize) this.extend();
     }
+
     delete(key) {
         let i = 0;
         let index = this.hashFunc(key, i++);
@@ -37,13 +45,29 @@ class DHashtable{
         }
         return undefined;
     }
+
     hashFunc(key, i = 0){
-        let index = (key % this.arraySize) + (i**2)
+        
+        if (typeof num === "number"){
+            let index = (key % this.arraySize) + (i)
         while(index >= this.arraySize){
             index -= this.arraySize
         }
         return index
+        } else if (typeof str === "string"){
+            let index = 0;
+            for(let j = 0; j < key.length; j++){
+                 index = key[i].charCodeAt(0) * j
+            }
+            index = index + (i**2)
+        while(index >= this.arraySize){
+            index -= this.arraySize
+        }
+        return index
+        }
+        
     }
+
     find(key){
         let i = 0;
         let index = this.hashFunc(key, i++);
@@ -55,6 +79,7 @@ class DHashtable{
         }
         return undefined;
     }
+
     extend(){
         this.arraySize = this.arraySize * 3;
         const newArray = new Array(this.arraySize);
@@ -71,6 +96,11 @@ class DHashtable{
         }
         this.array = newArray;
     }
+
+    forEach(func){
+        this.keys.forEach(key => func(key, this.find(key)));
+    }
+
     shrink(){
         this.arraySize /= 3;
         const newArray = new Array(this.arraySize);
@@ -87,7 +117,10 @@ class DHashtable{
         }
         this.array = newArray;
     }
-    getHash(){
+
+    get getHash(){
         return this.array
     }
+
+
 }
